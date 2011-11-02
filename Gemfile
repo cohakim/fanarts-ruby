028d972d7da55749c52a747534385cbfc863d21b
cohakim <cohakim@gmail.com>
2011-11-03 08:40:40 +0900

    デプロイ設定修正

--- Gemfile
+++ Gemfile
@@ -2,11 +2,11 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0'
 gem 'ruby-debug19', :require => 'ruby-debug'
+gem 'uglifier'
 gem 'jquery-rails'
 gem 'capistrano'
 gem 'sqlite3'
 gem 'minitest'
-gem 'annotate', :git => 'git://github.com/ctran/annotate_models.git'
 gem 'whenever', :require => false
 
 gem 'hpricot'
@@ -16,6 +16,10 @@ group :assets do
   gem 'sass-rails', "  ~> 3.1.0"
   gem 'coffee-rails', "~> 3.1.0"
   gem 'uglifier'
+end
+
+group :development do
+  gem 'annotate', :git => 'git://github.com/ctran/annotate_models.git'
 end
 
 group :test do
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,10 @@
+require "bundler/capistrano"
+
 set :application, "fanarts"
+set :user,        "app"
+set :use_sudo,    false
 
 set :scm, :git
-
 set :branch,      "develop"
 set :repository,  "/Users/cohakim/Public/Dropbox/projects/Rails/FanArts"
 set :deploy_via,  :copy
@@ -15,3 +18,14 @@ set :deploy_to,   "/var/www/app/fanarts/"
 role :web, "106.187.39.247"
 role :app, "106.187.39.247"
 role :db,  "106.187.39.247", :primary => true
+
+# set :default_environment, {
+#   'PATH' => "/home/app/.rbenv/shims:/home/app/.rbenv/bin:$PATH"
+# }
+set :bundle_gemfile,  "Gemfile"
+set :bundle_dir,      File.join(fetch(:shared_path), 'bundle')
+set :bundle_flags,    "--deployment --quiet"
+set :bundle_without,  [:development, :test]
+set :bundle_cmd,      "bundle"
+set :bundle_roles,    [:app]
+
