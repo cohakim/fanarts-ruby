83675a21b03e901b69387426ffd024935e54da87
cohakim <cohakim@gmail.com>
2011-11-04 05:05:59 +0900

    シーケンス番号を保存するのに十分なカラム長を確保できていなかったのを修正

--- db/migrate/20111026083630_create_topics.rb
+++ db/migrate/20111026083630_create_topics.rb
@@ -1,8 +1,8 @@
 class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
-      t.integer  :topic, :null => false
-      t.text     :sequences
+      t.integer    :topic, :null => false
+      t.mediumtext :sequences
       
       t.timestamps
     end
