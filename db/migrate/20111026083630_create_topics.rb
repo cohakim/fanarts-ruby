c0ee4f5f3276180e9d73fc94ddd8b77e34d82454
cohakim <cohakim@gmail.com>
2011-11-08 14:37:48 +0900

    medeiumtext型を指定して落ちていたのを修正

--- db/migrate/20111026083630_create_topics.rb
+++ db/migrate/20111026083630_create_topics.rb
@@ -2,9 +2,10 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.integer    :topic, :null => false
-      t.mediumtext :sequences
+      t.text       :sequences
       
       t.timestamps
     end
+    execute "ALTER TABLE topics MODIFY COLUMN sequences MEDIUMTEXT"
   end
 end
