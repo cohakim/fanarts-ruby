bc313709966bb1cb7fe9b7a1a859b1758d0e55f3
cohakim <cohakim@gmail.com>
2012-01-06 16:14:35 +0900

    bundler/capistrano設定完成版

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,8 @@ set :deploy_to,   "/var/www/app/fanarts/"
 # for bundler
 require "bundler/capistrano"
 set :bundle_gemfile,  "Gemfile"
+set :bundle_dir,      File.join(fetch(:shared_path), 'bundle')
+set :bundle_flags,    "--deployment --quiet"
 set :bundle_without,  [:development, :test]
 set :bundle_cmd,      "bundle"
 set :bundle_roles,    [:app]
